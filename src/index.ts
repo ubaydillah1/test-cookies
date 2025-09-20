852e3d5a33a68af4ebd4a98fa2f8e117f2c89950
ubaydillah1 <ubaydillah1737@gmail.com>
2025-09-20 07:44:27 +0700

    test

--- src/index.ts
+++ src/index.ts
@@ -2,34 +2,38 @@ import express from "express";
 import type { Request, Response } from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+
 const app = express();
 
+// Middleware
 app.use(express.json());
 app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "*",
+    origin: "https://e-learning-test-online.vercel.app", // FE domain
     credentials: true,
   })
 );
 
-app.get("/", (req: Request, res: Response) => {
+// Route set cookie
+app.get("/set-cookie", (req: Request, res: Response) => {
   res.cookie("token", "123456", {
     httpOnly: true,
     sameSite: "none",
-    // domain: ".vercel.app",
-    secure: true,
-  });
-
-  app.get("/profile", (req, res) => {
-    const token = req.cookies?.token || null;
-    res.json({ token });
+    secure: true, // harus HTTPS
   });
 
   res.json({ message: "Cookies Set" });
 });
 
+// Route profile (baca cookie)
+app.get("/profile", (req: Request, res: Response) => {
+  const token = req.cookies?.token || null;
+  res.json({ token });
+});
+
+// Start server
 app.listen(3002, () => {
   console.log("Server is running on port 3002");
 });
