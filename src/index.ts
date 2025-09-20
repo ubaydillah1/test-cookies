f3a40fc45389521ef527c2e87bdc828778480d20
ubaydillah1 <ubaydillah1737@gmail.com>
2025-09-20 07:21:16 +0700

    tst

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import express from "express";
 import type { Request, Response } from "express";
+import cookieParser from "cookie-parser";
 import cors from "cors";
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
+
 app.use(
   cors({
     origin: "https://e-learning-test-online.vercel.app",
